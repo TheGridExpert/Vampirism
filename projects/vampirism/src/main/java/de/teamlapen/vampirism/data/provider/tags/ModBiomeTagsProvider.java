e1583123f166f64e78289ff958825b1cb389e627
TheGridExpert <grid.write.here@gmail.com>
2026-07-07 20:06:38 +0300

    Make vampire forest structures generate only in the vampire forest and not in vampire biomes in general

--- projects/vampirism/src/main/java/de/teamlapen/vampirism/data/provider/tags/ModBiomeTagsProvider.java
+++ projects/vampirism/src/main/java/de/teamlapen/vampirism/data/provider/tags/ModBiomeTagsProvider.java
@@ -22,22 +22,29 @@ public class ModBiomeTagsProvider extends BiomeTagsProvider {
     @SuppressWarnings("unchecked")
     @Override
     protected void addTags(HolderLookup.@NotNull Provider holderProvider) {
-        tag(ModBiomeTags.HasStructure.HUNTER_TENT).addTags(BiomeTags.IS_BADLANDS, BiomeTags.IS_FOREST, BiomeTags.IS_TAIGA).add(Biomes.PLAINS, Biomes.DESERT, Biomes.MEADOW, Biomes.SNOWY_PLAINS, Biomes.SPARSE_JUNGLE).remove(Biomes.PALE_GARDEN);
         tag(ModBiomeTags.HasFaction.IS_FACTION_BIOME).addTags(ModBiomeTags.HasFaction.IS_VAMPIRE_BIOME, ModBiomeTags.HasFaction.IS_HUNTER_BIOME);
         tag(ModBiomeTags.HasFaction.IS_VAMPIRE_BIOME).add(ModBiomes.VAMPIRE_FOREST);
         tag(ModBiomeTags.HasFaction.IS_HUNTER_BIOME);
-        tag(ModBiomeTags.HasStructure.VAMPIRE_ALTAR).addTags(Tags.Biomes.IS_WASTELAND, Tags.Biomes.IS_PLATEAU, Tags.Biomes.IS_RARE, Tags.Biomes.IS_SPOOKY, Tags.Biomes.IS_SWAMP, ModBiomeTags.HasFaction.IS_VAMPIRE_BIOME).remove(ModBiomes.VELMORRA);
+
+        tag(ModBiomeTags.HasStructure.HUNTER_TENT).addTags(BiomeTags.IS_BADLANDS, BiomeTags.IS_FOREST, BiomeTags.IS_TAIGA).add(Biomes.PLAINS, Biomes.DESERT, Biomes.MEADOW, Biomes.SNOWY_PLAINS, Biomes.SPARSE_JUNGLE).remove(Biomes.PALE_GARDEN);
+        tag(ModBiomeTags.HasStructure.VAMPIRE_HUT).add(ModBiomes.VAMPIRE_FOREST);
+        tag(ModBiomeTags.HasStructure.VAMPIRE_DUNGEON).addTags(BiomeTags.IS_OVERWORLD);
+        tag(ModBiomeTags.HasStructure.VELMORRA_PORTAL).addTag(BiomeTags.IS_OVERWORLD);
         tag(ModBiomeTags.HasStructure.HUNTER_OUTPOST_PLAINS).addTags(Tags.Biomes.IS_PLAINS, BiomeTags.IS_FOREST).remove(Biomes.PALE_GARDEN);
         tag(ModBiomeTags.HasStructure.HUNTER_OUTPOST_DESERT).addTags(Tags.Biomes.IS_DESERT);
         tag(ModBiomeTags.HasStructure.HUNTER_OUTPOST_VAMPIRE_FOREST).add(ModBiomes.VAMPIRE_FOREST);
         tag(ModBiomeTags.HasStructure.HUNTER_OUTPOST_BADLANDS).addTags(BiomeTags.IS_BADLANDS);
-        tag(ModBiomeTags.HasStructure.VELMORRA_PORTAL).addTag(BiomeTags.IS_OVERWORLD);
+        tag(ModBiomeTags.HasStructure.VAMPIRE_ALTAR).addTags(Tags.Biomes.IS_WASTELAND, Tags.Biomes.IS_PLATEAU, Tags.Biomes.IS_RARE, Tags.Biomes.IS_SPOOKY, Tags.Biomes.IS_SWAMP, ModBiomeTags.HasFaction.IS_VAMPIRE_BIOME).remove(ModBiomes.VELMORRA);
+        tag(ModBiomeTags.HasStructure.MOTHER).add(ModBiomes.VAMPIRE_FOREST);
+        tag(ModBiomeTags.HasStructure.CRYPT).add(ModBiomes.VAMPIRE_FOREST);
+        tag(ModBiomeTags.HasStructure.DRACULA_CASTLE).add(ModBiomes.VELMORRA);
+
         tag(BiomeTags.IS_FOREST).add(ModBiomes.VAMPIRE_FOREST);
         tag(BiomeTags.IS_OVERWORLD).add(ModBiomes.VAMPIRE_FOREST);
         tag(Tags.Biomes.IS_DENSE_VEGETATION_OVERWORLD).add(ModBiomes.VAMPIRE_FOREST);
         tag(Tags.Biomes.IS_MAGICAL).add(ModBiomes.VAMPIRE_FOREST, ModBiomes.VELMORRA);
         tag(Tags.Biomes.IS_SPOOKY).add(ModBiomes.VAMPIRE_FOREST, ModBiomes.VELMORRA);
-        tag(ModBiomeTags.HasStructure.VAMPIRE_DUNGEON).addTags(BiomeTags.IS_OVERWORLD);
+
         tag(ModBiomeTags.HasSpawn.VAMPIRE).addTags(BiomeTags.IS_OVERWORLD);
         tag(ModBiomeTags.NoSpawn.VAMPIRE).addTags(ModBiomeTags.HasFaction.IS_FACTION_BIOME, Tags.Biomes.IS_UNDERGROUND, Tags.Biomes.IS_MUSHROOM);
         tag(ModBiomeTags.HasSpawn.ADVANCED_VAMPIRE).addTags(BiomeTags.IS_OVERWORLD);
@@ -46,10 +53,6 @@ public class ModBiomeTagsProvider extends BiomeTagsProvider {
         tag(ModBiomeTags.NoSpawn.HUNTER).addTags(ModBiomeTags.HasFaction.IS_FACTION_BIOME);
         tag(ModBiomeTags.HasSpawn.ADVANCED_HUNTER).addTags(BiomeTags.IS_OVERWORLD);
         tag(ModBiomeTags.NoSpawn.ADVANCED_HUNTER).addTags(ModBiomeTags.HasFaction.IS_FACTION_BIOME);
-        tag(ModBiomeTags.HasStructure.VAMPIRE_HUT).addTags(ModBiomeTags.HasFaction.IS_VAMPIRE_BIOME);
-        tag(ModBiomeTags.HasStructure.MOTHER).addTag(ModBiomeTags.HasFaction.IS_VAMPIRE_BIOME);
-        tag(ModBiomeTags.HasStructure.CRYPT).addTag(ModBiomeTags.HasFaction.IS_VAMPIRE_BIOME);
-        tag(ModBiomeTags.HasStructure.DRACULA_CASTLE).add(ModBiomes.VELMORRA);
         tag(Tags.Biomes.NO_DEFAULT_MONSTERS).add(ModBiomes.VAMPIRE_FOREST);
     }
 }
